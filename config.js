b7bd94f3466a96944acccf04093a8faeff779c04
Atika76 <139039295+Atika76@users.noreply.github.com>
2025-10-14 20:25:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Előre kitöltött beállítások – NE töltsd fel publikus repo-ba, ha zavar a kulcs nyilvánossága.
 window.__CONFIG__ = {
-  SUPABASE_URL: "https://bxtpnotswnwrbycvfypz.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4dHBub3Rzd253cmJ5Y3ZmeXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MTQ0NDcsImV4cCI6MjA2ODQ5MDQ0N30.CXEfo_8qmIYhkEZFdTsbl9ZB-PRTP6UK8EbIxxpSGZc",
+  SUPABASE_URL: "https://mvtjyxyzdwfngzucbwoy.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dGp5eHlkendmbmd6dWNid295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMTk1NDEsImV4cCI6MjA3NTg5NTU0MX0.vPVuiMRcEFIemX36NOm-_e1PRNs8YGKAvVGIQP4u7oY",
   BUCKET: "hirdetes-kepek",
   EDGE_FUNCTION_URL: ""
 };
