94b1823bea0cce943340b707045a919f9adfb585
Atika76 <139039295+Atika76@users.noreply.github.com>
2025-10-19 15:10:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// SmartMarket - Supabase + Gemini konfiguráció
+// ✅ SmartMarket - Supabase + Gemini konfiguráció
 window.__CONFIG__ = {
-  SUPABASE_URL: "https://mvtjyxyzdwfngzucbwoy.supabase.co",
+  SUPABASE_URL: "https://mvtjyxydzwfngzucbwoy.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dGp5eHlkendmbmd6dWNid295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMTk1NDEsImV4cCI6MjA3NTg5NTU0MX0.vPVuiMRcEFIemX36NOm-_e1PRNs8YGKAvVGIQP4u7oY",
   BUCKET: "hirdetes-kepek",
- EDGE_FUNCTION_URL: "https://mvtjyxyzdwfngzucbwoy.supabase.co/functions/v1/gemini-proxy"
+  EDGE_FUNCTION_URL: "https://mvtjyxydzwfngzucbwoy.supabase.co/functions/v1/gemini-proxy"
 };
